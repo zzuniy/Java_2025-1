8d212c29ca1db18189fc3b7cae8a92614569bf7c
zzuniy <heojueun050930@naver.com>
2025-03-19 19:54:16 +0900

    Java

---
+++ HeoJueun.java
@@ -0,0 +1,8 @@
+public class HeoJueun{
+    public static void main(String [] args){
+        System.out.println ("Name : 허주은");
+        System.out.println ("University:  한동대학교");
+        System.out.println ("Student Number : 22400790");
+        
+    }
+}
